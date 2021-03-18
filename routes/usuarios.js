85b32b8d5dd5052f0faea77eed12ffef213940f3
Fernando Cordón <fcordon@pin.upv.es>
2021-03-18 11:56:56 +0100

    Primer commit

---
+++ routes/usuarios.js
@@ -0,0 +1,24 @@
+
+const { Router } = require('express');
+const { usuariosGet, 
+        usuariosPut, 
+        usuariosPost, 
+        usuariosDelete, 
+        usuariosPatch } = require('../controllers/usuarios');
+
+const router = Router();
+
+ // endpoint 'localhost:8080/api'
+ router.get('/', usuariosGet );
+
+router.put('/:id', usuariosPut );
+
+router.post('/', usuariosPost );
+
+router.delete('/', usuariosDelete );
+
+router.patch('/', usuariosPatch );
+
+
+
+module.exports = router;
